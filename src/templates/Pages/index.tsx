dcb12251ebc3c619cdf3f39ba1f2f915f894e30a
unknown <thiagohd93@gmail.com>
2021-03-10 16:34:44 -0300

    GraphQL Code Generator

---
+++ src/templates/Pages/index.tsx
@@ -0,0 +1,22 @@
+import { CloseCircleOutline } from '@styled-icons/evaicons-outline';
+import LinkWrapper from 'components/LinkWrapper';
+import * as S from './styles';
+
+export type PageTemplateProps = {
+  heading: string;
+  body: string;
+};
+
+const PagesTemplate = ({ heading, body }: PageTemplateProps) => (
+  <S.Content>
+    <LinkWrapper href="/">
+      <CloseCircleOutline size={32} />
+    </LinkWrapper>
+    <S.Heading>{heading}</S.Heading>
+    <S.Body>
+      <div dangerouslySetInnerHTML={{ __html: body }} />
+    </S.Body>
+  </S.Content>
+);
+
+export default PagesTemplate;
